67a98570c2ee8438e0c8471089ba401996fd43f7
weiyuan <weiyuanv@sina.com>
2019-09-27 02:23:54 +0800

    第一次

---
+++ src/main/java/com/wei/test/Demo.java
@@ -0,0 +1,13 @@
+package com.wei.test;
+
+/**
+ * @version V1.0
+ * @author: weiyuan
+ * @date: 2019/9/27 2:13
+ * @description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
